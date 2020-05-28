ef65c4d8d2a964fd5e5b94371416a422fb512251
Vinod Kurup <vinod@kurup.com>
2020-05-28 15:46:01 -0400

    Fix comment

--- config/dev.exs
+++ config/dev.exs
@@ -60,8 +60,7 @@ config :voomex, VoomexWeb.Endpoint,
     ]
   ]
 
-# Do not include metadata nor timestamps in development logs
-config :logger, :console, format: "$time [$level] $message\n"
+config :logger, :console, format: "$date $time $metadata[$level] $message\n"
 
 # Set a higher stacktrace during development. Avoid configuring such
 # in production as building large stacktraces may be expensive.
